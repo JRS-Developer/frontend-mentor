c7dfd72bc2f14b3b53845050d267451b6a08e0f9
Jose Sanchez <sanchezjoserafael25@gmail.com>
2021-08-14 13:10:33 -0400

    Listo por fin la función para filtrar trabajos y renderizarlos correctamente, ahora solo faltan los estilos

--- src/reducer.ts
+++ src/reducer.ts
@@ -45,13 +45,13 @@ const createNewState = (
 	newFilterList: string[],
 	initialJobItems: JobInterface[]
 ): AppContextInterface => {
-	const newJobItems = getNewJobItems({
-		jobItems: initialJobItems,
-		filters: newFilterList,
-	});
 	if (newFilterList.length === 0) {
 		return { ...initialAppContext };
 	} else {
+		const newJobItems = getNewJobItems({
+			jobItems: initialJobItems,
+			filters: newFilterList,
+		});
 		return { ...state, jobItems: newJobItems, filters: newFilterList };
 	}
 };
@@ -67,38 +67,24 @@ const getNewJobItems = ({
 	jobItems,
 	filters,
 }: AppContextInterface): JobInterface[] => {
-	// Primer intento
-	// const newJobItems = jobItems.filter((job) => {
-	// 	let response: boolean[] = [];
-	// 	response.push(checkIfIncludes(job.languages, filters));
-	// 	response.push(checkIfIncludes(job.tools, filters));
-	// 	response.push(checkIfIncludes(job.role, filters));
-	// 	response.push(checkIfIncludes(job.level, filters));
-	// 	console.log(response);
-	// 	return response.every((r) => r === true);
-	// });
-	// return newJobItems;
-	// Segundo intento
-	// let newJobItems: JobInterface[] = [];
-	// filters.forEach((filter) => {
-	// 	newJobItems = [
-	// 		...newJobItems,
-	// 		...jobItems.filter((job) => {
-	// 			if (
-	// 				job.languages.includes(filter) ||
-	// 				job.tools.includes(filter) ||
-	// 				job.role === filter ||
-	// 				job.level === filter
-	// 			) {
-	// 				return true;
-	// 			} else {
-	// 				return false;
-	// 			}
-	// 		}),
-	// 	];
-	// });
-	// return newJobItems;
+	const filtersLength: number = filters.length;
 
-	return jobItems;
+	const newJobItems = jobItems.filter((job) => {
+		let matches: boolean[] = [];
+		filters.forEach((filter) => {
+			if (
+				job.languages.includes(filter) ||
+				job.tools.includes(filter) ||
+				job.role === filter ||
+				job.level === filter
+			) {
+				{
+					matches.push(true);
+				}
+			}
+		});
+		return matches.length === filtersLength;
+	});
+	return newJobItems;
 };
 export default reducer;
