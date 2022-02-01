d3bbdb8ffb32710884939d40b0d9d96d39dbfcd4
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-02-01 17:31:54 -0400

    First Commit

---
+++ backend/src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import config from './config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      load: [config],
+      isGlobal: true,
+    }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        dialect: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        database: configService.get('DB_NAME'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class AppModule {}
