6bbf545b1886d55ee0f8542f2f5dc01a58e861e2
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-02-17 19:44:06 -0400

    Importado el modulo de Likes para user los services de likes en el de comentarios

--- backend/src/comments/comments.module.ts
+++ backend/src/comments/comments.module.ts
@@ -4,9 +4,10 @@ import { CommentsController } from './comments.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Comment } from './models/comment.model';
 import { UsersModule } from '../users/users.module';
+import { LikesModule } from '../likes/likes.module';
 
 @Module({
-  imports: [SequelizeModule.forFeature([Comment]), UsersModule],
+  imports: [SequelizeModule.forFeature([Comment]), UsersModule, LikesModule],
   controllers: [CommentsController],
   providers: [CommentsService],
 })
