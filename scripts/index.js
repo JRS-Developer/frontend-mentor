283d1e6fd5b5ce2991a149de81a4d7680fb8a9cf
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-03-20 13:51:15 -0400

    feat: handled the submit btn and added some validations

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,42 @@
+"use strict";
 const ratingBtns = document.querySelectorAll(".rating-btn");
+const ratingItems = document.querySelectorAll(".rating-item");
 const submitBtn = document.getElementById("submit-btn");
 const ratingText = document.getElementById("rating-text");
 
-let selectedRating = 1;
+let defaultRating = 0;
+let selectedRating = defaultRating;
 
-const removeAllActive = () =>
-  ratingBtns.forEach((btn) => btn.classList.remove("active"));
+const addAllActive = (items) => items.forEach((i) => i.classList.add("active"));
+const removeAllActive = (items) =>
+  items.forEach((i) => i.classList.remove("active"));
 
 const handleRatingClick = (e) => {
   const btn = e.target;
-  selectedRating = btn.dataset.rating;
-  removeAllActive();
+  selectedRating = parseInt(btn.dataset.rating);
+
+  removeAllActive(ratingBtns);
   btn.classList.add("active");
+  submitBtn.disabled = false;
 };
 
 const handleSubmitClick = (e) => {
   e.preventDefault();
+
+  if (
+    selectedRating === defaultRating ||
+    !selectedRating ||
+    isNaN(selectedRating)
+  )
+    return (submitBtn.disabled = true);
+
   ratingText.innerText = selectedRating;
+  addAllActive(ratingItems);
 };
 
 ratingBtns.forEach((b) => b.addEventListener("click", handleRatingClick));
 submitBtn.addEventListener("click", handleSubmitClick);
+
+window.addEventListener("load", () => {
+  ratingText.innerText = selectedRating;
+});
