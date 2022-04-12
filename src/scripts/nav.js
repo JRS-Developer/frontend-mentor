588ad1d8f0684766c19b44a7adb17a43cd3d51b7
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-04-11 20:29:51 -0400

    feat: working a little to improve the accessibility

--- src/scripts/nav.js
+++ src/scripts/nav.js
@@ -1,10 +1,14 @@
 const navbarHamburger = document.getElementById("navbar-hamburger");
 const navbar = document.getElementById("navbar");
 
-console.log(navbarHamburger);
-
 navbarHamburger.addEventListener("click", () => {
-  console.log("hello");
   navbarHamburger.classList.toggle("active");
   navbar.classList.toggle("active");
+
+  // Update the expanded state of the navbar
+  const ariaExpanded = navbarHamburger.getAttribute("aria-expanded");
+  navbarHamburger.setAttribute(
+    "aria-expanded",
+    ariaExpanded === "true" ? "false" : "true"
+  );
 });
