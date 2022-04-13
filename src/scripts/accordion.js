4149f3eaae746f7499b70914ba7f55a72393450c
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-04-12 20:16:21 -0400

    feat: better accesibility to the accordion

--- src/scripts/accordion.js
+++ src/scripts/accordion.js
@@ -15,14 +15,16 @@
     element.classList.remove(classToRemove);
 
   const hideTriggers = (triggers) =>
-    triggers.forEach((trigger) =>
-      hideElement(trigger, triggersClassNames.active)
-    );
+    triggers.forEach((trigger) => {
+      hideElement(trigger, triggersClassNames.active);
+      trigger.setAttribute("aria-expanded", "false");
+    });
 
   const hidePanels = (panels) => {
     panels.forEach((panel) => {
       hideElement(panel, panelsClassNames.active);
       panel.style.maxHeight = 0;
+      panel.setAttribute("aria-hidden", "true");
     });
   };
 
@@ -32,12 +34,13 @@
   const showPanel = (panel) => {
     panel.classList.add(panelsClassNames.active);
     panel.style.maxHeight = panel.scrollHeight + "px";
+    panel.setAttribute("aria-hidden", "false");
   };
 
   const handleAccordionClick = (triggers, panels) => (e) => {
     if (e.target && e.target.classList.contains(triggersClassNames.normal)) {
       const trigger = e.target;
-      const panel = trigger.nextElementSibling;
+      const panel = trigger.parentElement.nextElementSibling;
 
       // If the panel is already active, hide it
       if (trigger.classList.contains(triggersClassNames.active)) {
